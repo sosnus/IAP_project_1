d6d533ddb58fa5c186b3161924e1f1ecce2f96c8
Wiktor <wiktor.muraszko@wp.pl>
2021-03-16 16:33:09 +0100

    intialcomit

---
+++ src/main/java/com/IAP/car_exchange/CarExchangeApplication.java
@@ -0,0 +1,22 @@
+package com.IAP.car_exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class CarExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarExchangeApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
---
+++ src/test/java/com/IAP/car_exchange/CarExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IAP.car_exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
